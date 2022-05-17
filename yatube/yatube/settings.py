37bc7dcb7213b10a38191f24d82de97790b13528
Александр Морев <a.morev@kryptonite.ru>
2022-05-17 21:52:08 +0300

    Add debug toolbar

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -32,6 +32,10 @@ ALLOWED_HOSTS = [
     "testserver",
 ]
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -46,6 +50,7 @@ INSTALLED_APPS = [
     "core.apps.CoreConfig",
     "about.apps.AboutConfig",
     "sorl.thumbnail",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -56,6 +61,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "yatube.urls"
